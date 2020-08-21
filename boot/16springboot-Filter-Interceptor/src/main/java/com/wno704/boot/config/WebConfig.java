63bcb81149b99cc990510e0567e6764fd2d969da
wu.danyang <wno704@126.com>
2020-08-21 17:33:32 +0800

    代码提交

---
+++ boot/16springboot-Filter-Interceptor/src/main/java/com/wno704/boot/config/WebConfig.java
@@ -0,0 +1,39 @@
+package com.wno704.boot.config;
+
+import com.wno704.boot.filter.TimeFilter;
+import com.wno704.boot.interceptor.TimeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private TimeInterceptor timeInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(timeInterceptor)
+                .excludePathPatterns("/index.html","/","/user/login"); //可以排除一些路径的拦截
+    }
+
+    @Bean
+    public FilterRegistrationBean timeFilter() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        TimeFilter timeFilter = new TimeFilter();
+        filterRegistrationBean.setFilter(timeFilter);
+
+        List<String> urlList = new ArrayList<>();
+        urlList.add("/*");
+
+        filterRegistrationBean.setUrlPatterns(urlList);
+        return filterRegistrationBean;
+    }
+}
