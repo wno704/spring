bc15dff85f76608244b7a74b9469f182d5adae1e
wu.danyang <wno704@126.com>
2020-08-21 17:35:58 +0800

    代码提交

---
+++ boot/19springboot-actuator/src/main/java/com/wno704/boot/ActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.wno704.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+}
